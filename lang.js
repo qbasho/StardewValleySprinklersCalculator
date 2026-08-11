3a0f0313361c83ece4a70025dc71673ea329d122
Qbasho <59530998+qbasho@users.noreply.github.com>
2026-08-11 11:40:41 +0200

    v1.0

---
+++ lang.js
@@ -0,0 +1,90 @@
+const dictionary = {
+    en: {
+        title: "SPRINKLERS CALCULATOR",
+        nozzle: "INSTALL PRESSURE NOZZLES",
+        sub: "(Increases range of every sprinkler)",
+        basic: "Sprinkler",
+        quality: "Quality Sprinkler",
+        iridium: "Iridium Sprinkler",
+        
+        copper: ["Copper Bar", "Copper Bars"],
+        iron: ["Iron Bar", "Iron Bars"],
+        gold: ["Gold Bar", "Gold Bars"],
+        quartz: ["Refined Quartz", "Refined Quartz"],
+        iri_bar: ["Iridium Bar", "Iridium Bars"],
+        battery: ["Battery Pack", "Battery Packs"],
+        
+        range: "Range",
+        tiles: "tiles",
+        boost: "NOZZLE!",
+        total: "Total watered tiles:",
+        wait: "Add sprinklers to see required resources...",
+        close: "Close"
+    },
+    pl: {
+        title: "Kalkulator Sprinklerów",
+        nozzle: "ZAMONTUJ DYSZE CIŚNIENIOWE",
+        sub: "(Zwiększa zasięg każdego zraszacza)",
+        basic: "Zraszacz",
+        quality: "Dobry Zraszacz",
+        iridium: "Irydowy Zraszacz",
+        
+        copper: ["Sztabka miedzi", "Sztabki miedzi", "Sztabek miedzi"],
+        iron: ["Sztabka żelaza", "Sztabki żelaza", "Sztabek żelaza"],
+        gold: ["Sztabka złota", "Sztabki złota", "Sztabek złota"],
+        quartz: ["Oczyszczony kwarc", "Oczyszczone kwarce", "Oczyszczonych kwarców"],
+        iri_bar: ["Sztabka irydu", "Sztabki irydu", "Sztabek irydu"],
+        battery: ["Bateria", "Baterie", "Baterii"],
+
+        range: "Zasięg",
+        tiles: "pól",
+        boost: "DYSZA!",
+        total: "Łącznie nawodnione grządki:",
+        wait: "Dodaj zraszacze by zobaczyć potrzebne surowce...",
+        close: "Zamknij"
+    },
+    de: {
+        title: "BEWÄSSERUNGSPLANER",
+        nozzle: "DRUCKDÜSEN INSTALLIEREN",
+        sub: "(Erhöht die Reichweite)",
+        basic: "Sprinkler",
+        quality: "Qualitätssprinkler",
+        iridium: "Iridium-Sprinkler",
+
+        copper: ["Kupferbarren", "Kupferbarren"],
+        iron: ["Eisenbarren", "Eisenbarren"],
+        gold: ["Goldbarren", "Goldbarren"],
+        quartz: ["Verfeinerter Quarz", "Verfeinerte Quarze"],
+        iri_bar: ["Iridiumbarren", "Iridiumbarren"],
+        battery: ["Batterie", "Batterien"],
+
+        range: "Reichweite",
+        tiles: "Felder",
+        boost: "DÜSE!",
+        total: "Gesamt:",
+        wait: "Sprinkler hinzufügen für Rohstoffe...",
+        close: "Schließen"
+    },
+    es: {
+        title: "PLANIFICADOR DE RIEGO",
+        nozzle: "BOQUILLAS A PRESIÓN",
+        sub: "(Aumenta el alcance)",
+        basic: "Aspersor",
+        quality: "Aspersor Calidad",
+        iridium: "Aspersor Iridio",
+
+        copper: ["Lingote de cobre", "Lingotes de cobre"],
+        iron: ["Lingote de hierro", "Lingotes de hierro"],
+        gold: ["Lingote de oro", "Lingotes de oro"],
+        quartz: ["Cuarzo refinado", "Cuarzos refinados"],
+        iri_bar: ["Lingote de iridio", "Lingotes de iridio"],
+        battery: ["Batería", "Baterías"],
+        
+        range: "Alcance",
+        tiles: "casillas",
+        boost: "BOQUILLA!",
+        total: "Total:",
+        wait: "Añade aspersores para ver recursos...",
+        close: "Cerrar"
+    }
+};
